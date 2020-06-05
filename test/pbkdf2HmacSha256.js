803d045fc061106d020490dd98e411922b69238c
Juanra Dikal <j.hernandez@upc.edu>
2020-06-05 20:14:59 +0200

    updated vector for pbkdf2

--- test/pbkdf2HmacSha256.js
+++ test/pbkdf2HmacSha256.js
@@ -11,11 +11,11 @@ const vectors = require('./vectors/pbkdf2HmacSha256')
 describe('testing pbkdf2HmacSha256', function () {
   this.timeout(360000)
   for (const vector of vectors) {
-    describe(`${vector.comment} : P=${vector.P}, S=${vector.S}, c=${vector.c}, dkLen=${vector.dkLen})`, function () {
+    describe(`${vector.comment} : ${JSON.stringify(vector.input)}`, function () {
       if ('error' in vector) {
         it(`should be rejected because of ${vector.error}`, async function () {
           try {
-            await _pkg.pbkdf2HmacSha256(vector.P, vector.S, vector.c, vector.dkLen)
+            await _pkg.pbkdf2HmacSha256(vector.input.P, vector.input.S, vector.input.c, vector.input.dkLen)
             throw new Error('should have failed')
           } catch (err) {
             chai.expect(err).to.be.instanceOf(vector.error)
@@ -23,7 +23,7 @@ describe('testing pbkdf2HmacSha256', function () {
         })
       } else {
         it(`should match ${vector.output}`, async function () {
-          const ret = await _pkg.pbkdf2HmacSha256(vector.P, vector.S, vector.c, vector.dkLen)
+          const ret = await _pkg.pbkdf2HmacSha256(vector.input.P, vector.input.S, vector.input.c, vector.input.dkLen)
           chai.expect(bigintConversion.bufToHex(ret)).to.equal(vector.output)
         })
       }
